1149386be4ee44e03b9a091de841be0f72c6dc78
dkdu22-flore <dkdu22@gmail.com>
2026-04-28 17:59:43 +0900

    feat : mino 기본 움직임 추가, minoprefab 추가

---
+++ Assets/Scripts/MiniGame_Tetris/TetrisBlock.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public Vector3 rotationPoint;
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        //블록 하강
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            previousTime = Time.time;
+        }
+
+
+        //블록 좌우
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+
+        //블록 회전
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
+            if (!ValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            }
+        }
+    }
+
+
+    //배경을 벗어나지 않도록 체크하는 메서드
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
