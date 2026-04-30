fdf748d07b4e151e886f64e01887b2b3564e6d2f
dkdu22-flore <dkdu22@gmail.com>
2026-04-30 13:19:06 +0900

    feat : 줄 삭제, 줄에 남은 블록 이동 구현.
    거의 완성

---
+++ Assets/Scripts/MiniGame_Tetris/SpawnTetromino.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetromino : MonoBehaviour
+{
+    public GameObject[] Tetrominoes;
+    private List<int> bag = new List<int>();
+
+    public static SpawnTetromino Instance;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        NewTetromino();
+    }
+    void Update()
+    {
+    }
+
+
+    // 다음 블록을 뽑는 메서드
+    public void NewTetromino()
+    {
+        if (bag.Count == 0)
+        {
+            FillAndShuffleBag();
+        }
+
+        // 주머니에서 맨 앞의 블록을 꺼내고 리스트에서 삭제
+        int index = bag[0];
+        bag.RemoveAt(0);
+
+        Instantiate(Tetrominoes[index], transform.position, Quaternion.identity);
+    }
+
+
+    // 주머니를 채우고 섞는 메서드
+    private void FillAndShuffleBag()
+    {
+        // 1. 주머니에 0번부터 6번까지 총 7개의 인덱스를 채워 넣음
+        for (int i = 0; i < 7; i++)
+        {
+            bag.Add(i);
+        }
+
+
+        // 2. Fisher - Yates 알고리즘으로 리스트를 무작위로 섞음
+        for (int i = 0; i < bag.Count; i++)
+        {
+            int randomIndex = Random.Range(i, bag.Count);
+            int temp = bag[i];
+            bag[i] = bag[randomIndex];
+            bag[randomIndex] = temp;
+        }
+    }
+}
--- Assets/Scripts/MiniGame_Tetris/TetrisBlock.cs
+++ Assets/Scripts/MiniGame_Tetris/TetrisBlock.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
@@ -7,6 +8,8 @@ public class TetrisBlock : MonoBehaviour
     public float fallTime = 0.8f;
     public static int height = 20;
     public static int width = 10;
+    private static Transform[,] grid = new Transform[width, height];
+   
    
     void Start()
     {
@@ -23,6 +26,10 @@ public class TetrisBlock : MonoBehaviour
             if (!ValidMove())
             {
                 transform.position += new Vector3(0, 1, 0);
+                AddToGrid();
+                CheckForLines();
+                this.enabled = false;
+                SpawnTetromino.Instance.NewTetromino();
             }
             previousTime = Time.time;
         }
@@ -58,6 +65,68 @@ public class TetrisBlock : MonoBehaviour
         }
     }
 
+    private void CheckForLines()
+    {
+        for (int i = height-1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+
+                i++;
+            }
+        }
+    }
+
+
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+    private void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j,y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+
+        }
+    }
 
     //배경을 벗어나지 않도록 체크하는 메서드
     bool ValidMove()
@@ -68,6 +137,11 @@ public class TetrisBlock : MonoBehaviour
             int roundedY = Mathf.RoundToInt(children.transform.position.y);
 
             if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+
+            if (grid[roundedX, roundedY] != null)
             {
                 return false;
             }
